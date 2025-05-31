f8c63aa48cf2ef841bdb4d44ddf3cd27a666aee2
oPequeno <151648782+EvertonBertagnoli@users.noreply.github.com>
2025-05-31 10:38:56 -0300

    Add files via upload

---
+++ TarefaCalculoMedia.java
@@ -0,0 +1,24 @@
+package tarefa_classes;
+
+public class TarefaCalculoMedia {
+	
+	public static void main (String[] arg) {
+		
+		int nota1 = 8;
+		int nota2 = 5;
+		int nota3 = 8;
+		int nota4 = 6;
+		
+		int notaSoma = nota1 + nota2 + nota3 + nota4;
+		int notaMedia = notaSoma / 4;
+		
+		System.out.println("A soma das notas é: " + notaSoma);
+		System.out.println("E a media delas é: " + notaMedia);
+		
+		
+		
+				
+	}
+	
+
+}
